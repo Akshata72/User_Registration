44fd3c46606db946e15ab697635627e766d066a0
Akshata72 <sableakshata880@gmail.com>
2022-03-31 14:10:59 +0530

    Added:UC2-Valid_Last_Name

--- User Registration/Program.cs
+++ User Registration/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System.Text.RegularExpressions;
-namespace UserReg
+namespace UserRegex
 {
     class UserRegex
     {
         static void Main(string[] args)
         {
+            int Option = 0;
             UserRegistration userRegistration = new UserRegistration();
-            Console.WriteLine("Enter a First name:");
-            string FirstName = Console.ReadLine();
-            userRegistration.ValidFirstName(FirstName);
+            do
+            {
+                Console.WriteLine("Enter 1 for First Name:");
+                Console.WriteLine("Enter 2 for Last Name:");
+                Console.WriteLine("Enter 0 to Exit:");
+                try
+                {
+                    Option = int.Parse(Console.ReadLine());
+                    switch (Option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter a First name:");
+                            string FirstName = Console.ReadLine();
+                            userRegistration.ValidFirstName(FirstName);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter a First name:");
+                            string LastName = Console.ReadLine();
+                            userRegistration.ValidLastName(LastName);
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please Choose option");
+                }
+            }
+            while (Option != 0);
         }
     }
 }
--- User Registration/User_Registration.cs
+++ User Registration/User_Registration.cs
@@ -4,22 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-namespace UserReg
+namespace UserRegex
 {
     public class UserRegistration
     {
         public Regex FirstNameRegex = new Regex(@"^[A-Za-z]{2,}$");
+        public Regex LastNameRegex = new Regex(@"^[A-Za-z]{2,}$");
         public void ValidFirstName(string FirstName)
         {
-            Console.WriteLine("First Name:" + FirstName);
+            Console.WriteLine("\nFirst Name:" + FirstName);
             if (FirstNameRegex.IsMatch(FirstName))
             {
-                Console.WriteLine("First Name is valid");
+                Console.WriteLine("First Name is valid\n");
             }
             else
             {
-                Console.WriteLine("First name is invalid");
+                Console.WriteLine("First Name is invalid\n");
+            }
+        }
+        public void ValidLastName(string LastName)
+        {
+            Console.WriteLine("\nLast Name:" + LastName);
+            if (LastNameRegex.IsMatch(LastName))
+            {
+                Console.WriteLine("Last Name is valid\n");
+            }
+            else
+            {
+                Console.WriteLine("Last Name is invalid\n");
             }
         }
     }
 }
+
