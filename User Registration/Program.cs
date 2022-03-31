52fae27814d8de44d40c2423c810d09163140bc4
Akshata72 <sableakshata880@gmail.com>
2022-03-31 08:38:18 +0530

    Added:UC1-Valid_First_Name

---
+++ User Registration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+namespace UserReg
+{
+    class UserRegex
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            Console.WriteLine("Enter a First name:");
+            string FirstName = Console.ReadLine();
+            userRegistration.ValidFirstName(FirstName);
+        }
+    }
+}
---
+++ User Registration/User_Registration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace UserReg
+{
+    public class UserRegistration
+    {
+        public Regex FirstNameRegex = new Regex(@"^[A-Za-z]{2,}$");
+        public void ValidFirstName(string FirstName)
+        {
+            Console.WriteLine("First Name:" + FirstName);
+            if (FirstNameRegex.IsMatch(FirstName))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First name is invalid");
+            }
+        }
+    }
+}
